161a5b7b329b9571fb3427aa4a43a06e9628fc71
Summer Kim <dasomi6385@gmail.com>
2023-03-20 17:49:06 +0100

    Initial Commit

---
+++ basicTutorial/basicTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicTutorialApp.swift
+//  basicTutorial
+//
+//  Created by dasom kim on 2023/03/20.
+//
+
+import SwiftUI
+
+@main
+struct basicTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
