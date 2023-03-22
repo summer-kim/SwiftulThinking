9d68fb8a94ec808392a76c3e71252f169d97a874
Summer Kim <dasomi6385@gmail.com>
2023-03-22 18:32:45 +0100

    Swift tutorial for  overlay, background

--- basicTutorial/FrameBootcamp.swift
+++ basicTutorial/FrameBootcamp.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct FrameBootcamp: View {
     var body: some View {
-        Text("Hello, Summer!")
+        Circle()
+            .fill(
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.indigo, Color.purple]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            )
+            .frame(width: 80, height: 80)
+        
+            .overlay(
+                Image(systemName: "moon.stars")
+                    .font(.system(size: 45))
+                    .foregroundColor(Color.white)
+            )
+            
+            .overlay(
+                Circle()
+                    .fill(Color.teal).opacity(0.9)
+                    .frame(width: 30, height: 30)
+                    .overlay(Text("2")
+                        .font(.headline)
+                        .foregroundColor(Color.white))
+                ,alignment: .bottomTrailing
+            )
+        
+//        Text("Hello, Summer!")
+//            .background(Color.pink)
+//
+//            .frame(width: 150, height: 100, alignment: .trailing)
+//            .background(Color.yellow)
+//
+//            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+//            .background(Color.indigo).opacity(0.5)
+//
+//            .overlay(
+//                Circle().fill(Color.orange).frame(width: 200)
+            //background는 아래로 stack되고 overlay는 뒤에 적었어도 위로 올라감
     }
 }
 
